430dd1b22d64f6c8c0c792e42d1aa47f18daf4eb
Yunier Yau <yauyunier@gmail.com>
2022-11-30 11:51:46 -0500

    models dir creates and database connection established successfully

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 app = express();
 const path = require('path')
+const mongoose = require('mongoose')
+const Spot = require("./models/spot")
+
+mongoose
+  .connect("mongodb://localhost:27017/PanamaVacationSpot")
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 app.set("view engine", 'ejs')
 app.set('views', path.join(__dirname,'views'))
 
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
+  const spot = new Spot({title:'Chica', description:'vista preciosa y es totalmente gratis'})
+  await spot.save()
+  console.log(spot)
   res.render('home')
 });
 
