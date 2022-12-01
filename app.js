1629bafbc2e1dd482f8b80317b0b1d2182cb5806
Yunier Yau <yauyunier@gmail.com>
2022-12-01 08:59:27 -0500

    Spot delete available (Full CRUD $ Restful completed)

--- app.js
+++ app.js
@@ -35,10 +35,6 @@ app.post("/spots", async (req, res) => {
   res.redirect("/spots")
 });
 
-app.put("/spots/:id", async (req, res) => {
-  await Spot.findByIdAndUpdate(req.params.id, req.body.spot)
-  res.redirect("/spots")
-});
 
 app.get("/spots/new", (req, res) => {
   res.render("./spots/new");
@@ -47,6 +43,16 @@ app.get("/spots/new", (req, res) => {
 app.get("/spots/:id", async (req, res) => {
   const spot = await Spot.findById(req.params.id);
   res.render("./spots/show", {spot});
+});
+
+app.put("/spots/:id", async (req, res) => {
+  await Spot.findByIdAndUpdate(req.params.id, req.body.spot)
+  res.redirect(`/spots/${req.params.id}`)
+});
+
+app.delete("/spots/:id", async (req, res) => {
+  await Spot.findByIdAndDelete(req.params.id)
+  res.redirect("/spots")
 });
 
 app.get("/spots/:id/edit", async (req, res) => {
