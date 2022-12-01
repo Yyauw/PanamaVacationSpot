8ca7b035208f709f06016b45bba2cc8b44f1c648
Yunier Yau <yauyunier@gmail.com>
2022-12-01 07:59:35 -0500

    spot list and show page added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 app = express();
-const path = require('path')
-const mongoose = require('mongoose')
-const Spot = require("./models/spot")
+const path = require("path");
+const mongoose = require("mongoose");
+const Spot = require("./models/spot");
 
 mongoose
   .connect("mongodb://localhost:27017/PanamaVacationSpot")
@@ -13,15 +13,21 @@ mongoose
     console.log(err);
   });
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.set("view engine", 'ejs')
-app.set('views', path.join(__dirname,'views'))
+app.get("/", async (req, res) => {
+  res.render("home");
+});
 
-app.get("/", async(req, res) => {
-  const spot = new Spot({title:'Chica', description:'vista preciosa y es totalmente gratis'})
-  await spot.save()
-  console.log(spot)
-  res.render('home')
+app.get("/spots", async (req, res) => {
+  const spots = await Spot.find({});
+  res.render("./spots/index", {spots});
+});
+
+app.get("/spots/:id", async (req, res) => {
+  const spot = await Spot.findById(req.params.id);
+  res.render("./spots/show", {spot});
 });
 
 app.listen("3000", () => {
