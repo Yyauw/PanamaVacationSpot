070165f19d7521856c66b4928104174bffa07f60
Yunier Yau <yauyunier@gmail.com>
2022-12-04 11:18:11 -0500

    Async error Catching implemented and custom error class created

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const Spot = require("./models/spot");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 
 mongoose
   .connect("mongodb://localhost:27017/PanamaVacationSpot")
@@ -32,34 +34,58 @@ app.get("/spots", async (req, res) => {
   res.render("./spots/index", { spots });
 });
 
-app.post("/spots", async (req, res) => {
-  const spot = new Spot(req.body.spot);
-  await spot.save();
-  res.redirect("/spots");
-});
+app.post(
+  "/spots",
+  catchAsync(async (req, res, next) => {
+    const spot = new Spot(req.body.spot);
+    await spot.save();
+    res.redirect("/spots");
+  })
+);
 
 app.get("/spots/new", (req, res) => {
   res.render("./spots/new");
 });
 
-app.get("/spots/:id", async (req, res) => {
-  const spot = await Spot.findById(req.params.id);
-  res.render("./spots/show", { spot });
+app.get(
+  "/spots/:id",
+  catchAsync(async (req, res) => {
+    const spot = await Spot.findById(req.params.id);
+    res.render("./spots/show", { spot });
+  })
+);
+
+app.put(
+  "/spots/:id",
+  catchAsync(async (req, res) => {
+    await Spot.findByIdAndUpdate(req.params.id, req.body.spot);
+    res.redirect(`/spots/${req.params.id}`);
+  })
+);
+
+app.delete(
+  "/spots/:id",
+  catchAsync(async (req, res) => {
+    await Spot.findByIdAndDelete(req.params.id);
+    res.redirect("/spots");
+  })
+);
+
+app.get(
+  "/spots/:id/edit",
+  catchAsync(async (req, res) => {
+    const spot = await Spot.findById(req.params.id);
+    res.render("./spots/edit", { spot });
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
-app.put("/spots/:id", async (req, res) => {
-  await Spot.findByIdAndUpdate(req.params.id, req.body.spot);
-  res.redirect(`/spots/${req.params.id}`);
-});
-
-app.delete("/spots/:id", async (req, res) => {
-  await Spot.findByIdAndDelete(req.params.id);
-  res.redirect("/spots");
-});
-
-app.get("/spots/:id/edit", async (req, res) => {
-  const spot = await Spot.findById(req.params.id);
-  res.render("./spots/edit", { spot });
+app.use((err, req, res, next) => {
+  const { message = "Something went wrong", statusCode = 500 } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen("3000", () => {
