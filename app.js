8711af396de9039ca1cf76cccfdb42fbe7b4501a
Yunier Yau <yauyunier@gmail.com>
2022-12-10 11:23:55 -0500

    Public dir added && express-session septup done

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
-const Spots = require('./routes/spots')
-const Reviews = require('./routes/reviews')
+const Spots = require("./routes/spots");
+const Reviews = require("./routes/reviews");
+const session = require('express-session');
 
 mongoose
   .connect("mongodb://localhost:27017/PanamaVacationSpot")
@@ -24,9 +25,24 @@ app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/spots', Spots);
-app.use('/spots/:id/review', Reviews)
+const sessionConfig = {
+  secret:'badSecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig))
+
+app.use("/spots", Spots);
+app.use("/spots/:id/review", Reviews);
+
+
 
 app.get("/", async (req, res) => {
   res.render("home");
