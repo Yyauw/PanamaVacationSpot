0e45b927ef5d9d466727aa783276b4c6a4e1c535
Yunier Yau <yauyunier@gmail.com>
2022-12-11 14:06:36 -0500

    Login routes and logic added

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router()
 const User = require('../models/user')
 const catchAsync = require('../utils/catchAsync')
+const passport = require('passport')
 
 router.get('/register', (req,res)=>{
     res.render('./user/register')
@@ -19,5 +20,14 @@ try {
     res.redirect('/register')
 }
 }))
+
+router.get('/login', (req,res)=>{
+    res.render('./user/login')
+})
+
+router.post('/login', passport.authenticate('local', {failureFlash:true, failureRedirect:'/login'}), (req,res)=>{
+    req.flash('success', 'Welcome back!')
+    res.redirect('/spots')
+})
 
 module.exports = router;
