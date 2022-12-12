43e2dab5f63bddb54c926eec2bfb2caa733d4bd4
Yunier Yau <yauyunier@gmail.com>
2022-12-12 14:20:21 -0500

    Navbar login, singup and logout option added, register feedback updated

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  res.locals.userSession = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,55 @@
-const express = require('express');
-const router = express.Router()
-const User = require('../models/user')
-const catchAsync = require('../utils/catchAsync')
-const passport = require('passport')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+const passport = require("passport");
 
-router.get('/register', (req,res)=>{
-    res.render('./user/register')
-})
+router.get("/register", (req, res) => {
+  res.render("./user/register");
+});
 
-router.post('/register', catchAsync(async(req,res)=>{
-try {
-    const {email, username, password}= req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to PanamaVacationSpot!')
-    res.redirect('/spots')
-} catch (e) {
-    req.flash('error', e.message)
-    res.redirect('/register')
-}
-}))
+router.post(
+  "/register",
+  catchAsync(async (req, res, next) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.login(registeredUser, err =>{
+        if(err) return next(err);
+        req.flash("success", "Welcome to PanamaVacationSpot!");
+        res.redirect("/spots");
+      })
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/register");
+    }
+  })
+);
 
-router.get('/login', (req,res)=>{
-    res.render('./user/login')
-})
+router.get("/login", (req, res) => {
+  res.render("./user/login");
+});
 
-router.post('/login', passport.authenticate('local', {failureFlash:true, failureRedirect:'/login'}), (req,res)=>{
-    req.flash('success', 'Welcome back!')
-    res.redirect('/spots')
-})
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/spots");
+  }
+);
+
+router.get("/logout", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) return next(err);
+    req.flash("success", "Goodbye!");
+    res.redirect("/spots");
+  });
+
+});
 
 module.exports = router;
