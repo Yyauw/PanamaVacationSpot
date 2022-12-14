ce311054f5804ba67fbc1d83a5dd51e499aade7c
Yunier Yau <yauyunier@gmail.com>
2022-12-14 14:00:24 -0500

    User controller added

--- routes/users.js
+++ routes/users.js
@@ -3,29 +3,11 @@ const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
+const userController = require("../controller/userController");
 
-router.get("/register", (req, res) => {
-  res.render("./user/register");
-});
+router.get("/register", userController.registerForm);
 
-router.post(
-  "/register",
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        req.flash("success", "Welcome to PanamaVacationSpot!");
-        res.redirect("/spots");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/register");
-    }
-  })
-);
+router.post("/register", catchAsync(userController.registerUser));
 
 router.get("/login", (req, res) => {
   res.render("./user/login");
@@ -36,22 +18,11 @@ router.post(
   passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/login",
-    keepSessionInfo:true
+    keepSessionInfo: true,
   }),
-  (req, res) => {
-    const backUrl = req.session.backTo || "/spots";
-    delete req.session.backTo;
-    req.flash("success", "Welcome back!");
-    res.redirect(backUrl);
-  }
+  userController.login
 );
 
-router.get("/logout", (req, res, next) => {
-  req.logout(function (err) {
-    if (err) return next(err);
-    req.flash("success", "Goodbye!");
-    res.redirect("/spots");
-  });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
