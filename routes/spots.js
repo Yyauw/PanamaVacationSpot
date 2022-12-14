99c58fa9d4f5a3d354c7714d5f78f7fb5abd12c7
Yunier Yau <yauyunier@gmail.com>
2022-12-14 13:35:35 -0500

    Refactoring to spots controller

--- routes/spots.js
+++ routes/spots.js
@@ -3,78 +3,41 @@ const Spot = require("../models/spot");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateSpot } = require("../middleware");
+const spotController = require("../controller/spotsController");
 
-router.get("/", async (req, res) => {
-  const spots = await Spot.find({});
-  res.render("./spots/index", { spots });
-});
+router.get("/", spotController.index);
 
 router.post(
   "/",
   isLoggedIn,
   validateSpot,
-  catchAsync(async (req, res, next) => {
-    const spot = new Spot(req.body.spot);
-    spot.author = req.user._id;
-    await spot.save();
-    req.flash("success", "Successfully made spot!");
-    res.redirect(`/spots/${spot._id}`);
-  })
+  catchAsync(spotController.createSpot)
 );
 
-router.get("/new", isLoggedIn, (req, res) => {
-  res.render("./spots/new");
-});
+router.get("/new", isLoggedIn, spotController.newSpot);
 
-router.get(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const spot = await Spot.findById(req.params.id)
-      .populate({ path: "reviews", populate: { path: "author" } })
-      .populate("author");
-    if (!spot) {
-      req.flash("error", "Spot not found!");
-      return res.redirect("/spots");
-    }
-    res.render("./spots/show", { spot });
-  })
-);
+router.get("/:id", catchAsync(spotController.showSpot));
 
 router.put(
   "/:id",
   isLoggedIn,
   isAuthor,
   validateSpot,
-  catchAsync(async (req, res) => {
-    await Spot.findByIdAndUpdate(req.params.id, req.body.spot);
-    req.flash("success", "Successfully updated spot!");
-    res.redirect(`/spots/${req.params.id}`);
-  })
+  catchAsync(spotController.editSpot)
 );
 
 router.delete(
   "/:id",
   isLoggedIn,
   isAuthor,
-  catchAsync(async (req, res) => {
-    await Spot.findByIdAndDelete(req.params.id);
-    req.flash("success", "Successfully deleted spot!");
-    res.redirect("/spots");
-  })
+  catchAsync(spotController.deleteSpot)
 );
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
-  catchAsync(async (req, res) => {
-    const spot = await Spot.findById(req.params.id);
-    if (!spot) {
-      req.flash("error", "Spot not found!");
-      return res.redirect("/spots");
-    }
-    res.render("./spots/edit", { spot });
-  })
+  catchAsync(spotController.editForm)
 );
 
 module.exports = router;
