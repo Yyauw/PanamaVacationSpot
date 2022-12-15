4b8df50cae135cc800919f5037691e32585872b4
Yunier Yau <yauyunier@gmail.com>
2022-12-15 14:19:30 -0500

    Spots routes restructured

--- routes/spots.js
+++ routes/spots.js
@@ -5,33 +5,18 @@ const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateSpot } = require("../middleware");
 const spotController = require("../controller/spotsController");
 
-router.route("/").get(spotController.index);
-
-router.post(
-  "/",
-  isLoggedIn,
-  validateSpot,
-  catchAsync(spotController.createSpot)
-);
+router
+  .route("/")
+  .get(spotController.index)
+  .post(isLoggedIn, validateSpot, catchAsync(spotController.createSpot));
 
 router.get("/new", isLoggedIn, spotController.newSpot);
 
-router.get("/:id", catchAsync(spotController.showSpot));
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateSpot,
-  catchAsync(spotController.editSpot)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(spotController.deleteSpot)
-);
+router
+  .route("/:id")
+  .get(catchAsync(spotController.showSpot))
+  .put(isLoggedIn, isAuthor, validateSpot, catchAsync(spotController.editSpot))
+  .delete(isLoggedIn, isAuthor, catchAsync(spotController.deleteSpot));
 
 router.get(
   "/:id/edit",
