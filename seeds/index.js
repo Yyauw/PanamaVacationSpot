9ffa9035b26757a876f81184525f5cc8a0960aba
Yunier Yau <yauyunier@gmail.com>
2022-12-23 14:22:39 -0500

    Cluster map added to index page

--- seeds/index.js
+++ seeds/index.js
@@ -17,24 +17,28 @@ async function createSpot() {
   for (let x = 0; x < 50; x++) {
     const random100 = Math.floor(Math.random() * 563);
     const randomplaces = Math.floor(Math.random() * places.length);
-    const priceGenerator = Math.floor(10 +Math.random() * 50);
+    const priceGenerator = Math.floor(10 + Math.random() * 50);
     const spot = new Spot({
-      author:'639625e34ebc12897aad1daf',
+      author: "639625e34ebc12897aad1daf",
       location: `${cities[random100].city}, ${cities[random100].admin_name}`,
       title: `${cities[random100].city} ${places[randomplaces]}`,
-      geometry: { type: 'Point', coordinates: [ -80.826673508, 8.049286578 ] },
-      images:[
+      geometry: {
+        type: "Point",
+        coordinates: [cities[random100].lng, cities[random100].lat],
+      },
+      images: [
         {
-          url:'https://source.unsplash.com/collection/11649432/1600x900',
-          filename:'jijija'
+          url: "https://source.unsplash.com/collection/11649432/1600x900",
+          filename: "jijija",
         },
         {
-          url:'https://source.unsplash.com/collection/9457892/1600x900',
-          filename:'jijija2'
-        }
+          url: "https://source.unsplash.com/collection/9457892/1600x900",
+          filename: "jijija2",
+        },
       ],
-      price:priceGenerator,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit quas doloremque saepe, ullam, itaque, necessitatibus explicabo modi ratione inventore qui ducimus sapiente minima iste odit commodi! Magnam quasi inventore ratione'
+      price: priceGenerator,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit quas doloremque saepe, ullam, itaque, necessitatibus explicabo modi ratione inventore qui ducimus sapiente minima iste odit commodi! Magnam quasi inventore ratione",
     });
     await spot.save();
   }
